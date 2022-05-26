07865c4aca4b9088933437b0590f23e5ad760a47
Caleb Noble <uniqisuniq@outlook.com>
2022-05-26 06:07:11 +0530

    Fix

--- corvus_mido.mk
+++ corvus_mido.mk
@@ -26,7 +26,7 @@ $(call inherit-product, vendor/corvus/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := mido
-PRODUCT_NAME := lineage_mido
+PRODUCT_NAME := corvus_mido
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 4
 PRODUCT_MANUFACTURER := Xiaomi
