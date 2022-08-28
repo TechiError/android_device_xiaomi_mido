758168971a0992089d831c90d8d39cae1bce12d8
iam-pro <iam-pro@users.noreply.github.com>
2022-08-28 08:51:23 +0000

    mido: Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -8,3 +8,7 @@ camera_branch=12.1
 rm -rf $camera_dir &&
 git clone --depth=1 https://github.com/GrapheneOS/platform_external_Camera.git -b $camera_branch $camera_dir &&
 mv $camera_dir/prebuilt/Camera.apk "$DIR"/GrapheneCamera/ || exit $?
+echo 'Cloning Kernel tree..'
+git clone https://github.com/alone0316/kernel_mido kernel/xiaomi/mido
+echo 'Cloning Vendor tree..'
+git clone https://github.com/NRanjan-17/vendor_xiaomi_mido vendor/xiaomi/mido
