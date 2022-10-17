6214306ea71a43629ec23e0b81fc18e6a8ec230a
iam-pro <iam-pro@users.noreply.github.com>
2022-10-17 16:43:13 +0000

    mido: Update vendorsetup and remove graphene camera completely

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,13 +1,5 @@
 #!/usr/bin/env bash
 
-DIR="$(cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd -P)"
-
-echo 'Downloading latest prebuilt Camera.apk from GrapheneOS...'
-camera_dir=/tmp/GrapheneOSCamera
-camera_branch=12.1
-rm -rf $camera_dir &&
-git clone --depth=1 https://github.com/GrapheneOS/platform_external_Camera.git -b $camera_branch $camera_dir &&
-mv $camera_dir/prebuilt/Camera.apk "$DIR"/GrapheneCamera/ || exit $?
 echo 'Cloning Kernel tree..'
 git clone https://github.com/alone0316/kernel_mido kernel/xiaomi/mido
 echo 'Cloning Vendor tree..'
